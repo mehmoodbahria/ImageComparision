d7d5f8a5400b5f561d1dfc8c868a79f86aabb6a6
Lion MAC <lionmac@Lions-Mac-Pro.local>
2012-06-05 17:35:36 +0500

    Initial Commit

---
+++ ImageComparisionTests/ImageComparisionTests.h
@@ -0,0 +1,13 @@
+//
+//  ImageComparisionTests.h
+//  ImageComparisionTests
+//
+//  Created by Lion MAC on 6/5/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface ImageComparisionTests : SenTestCase
+
+@end
---
+++ ImageComparisionTests/ImageComparisionTests.m
@@ -0,0 +1,32 @@
+//
+//  ImageComparisionTests.m
+//  ImageComparisionTests
+//
+//  Created by Lion MAC on 6/5/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "ImageComparisionTests.h"
+
+@implementation ImageComparisionTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in ImageComparisionTests");
+}
+
+@end
